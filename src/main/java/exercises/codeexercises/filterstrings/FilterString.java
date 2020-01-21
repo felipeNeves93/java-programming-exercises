8f48af35636cb5ff97b8901e0f282c1fc38200d7
Felipe Neves <felipe.alneves@outlook.com>
2020-01-20 21:31:54 -0300

    Updated .gitignore

--- src/main/java/exercises/codeexercises/filterstrings/FilterString.java
+++ src/main/java/exercises/codeexercises/filterstrings/FilterString.java
@@ -1,4 +1,12 @@
 package exercises.codeexercises.filterstrings;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FilterString {
+    public List<String> filterStartWithALetterAndThreeLettersOnly(List<String> words) {
+        return words.stream()
+                .filter(word -> word.startsWith("a") && word.length() == 3)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/exercises/codeexercises/filterstrings/FilterStringTest.java
+++ src/main/java/exercises/codeexercises/filterstrings/FilterStringTest.java
@@ -1,4 +1,29 @@
 package exercises.codeexercises.filterstrings;
 
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
 public class FilterStringTest {
+
+    private FilterString filterString = new FilterString();
+
+    @Test
+    public void filterStartWithALetterAndThreeLettersOnly_0_withoutAnyWordEligible() {
+        List<String> words = Arrays.asList("felipe", "neves", "test");
+        assertEquals(0, filterString.filterStartWithALetterAndThreeLettersOnly(words).size());
+    }
+
+    @Test
+    public void filterStartWithALetterAndThreeLettersOnly_air_withOneWordEligible() {
+        final String EXPECTED_WORD = "air";
+        List<String> words = Arrays.asList("felipe", "air", "guitar");
+
+        List<String> filteredWords = filterString.filterStartWithALetterAndThreeLettersOnly(words);
+        assertEquals(1, filteredWords.size());
+        assertEquals(EXPECTED_WORD, filteredWords.get(0));
+    }
 }
